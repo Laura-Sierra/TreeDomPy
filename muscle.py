5526e08daefc15ea36190ae4c90cc55f218a197d
Laura Sierra <59922409+Laura-Sierra@users.noreply.github.com>
2020-01-19 22:36:42 +0100

    Update muscle.py

--- muscle.py
+++ muscle.py
@@ -14,7 +14,7 @@ def muscle(hits_fasta):
 	"""Muscle alignement and tree.nwx generation 
     Args: hits in fasta format
     """
-	muscle_process = Popen(['muscle','-in',hits_fasta, "-out", "muscle_result.fasta", "-tree2", "tree.nwx"], stdout=PIPE, stderr=PIPE)
+	muscle_process = Popen(['muscle','-in',hits_fasta, "-out", "muscle_result.fasta", "-cluster", "neighborjoining", "-tree2", "tree.nwx"], stdout=PIPE, stderr=PIPE)
 	muscle_error = muscle_process.stderr.read().decode('utf-8')
 	muscle_result = muscle_process.stdout.read().decode('utf-8')
 	muscle_process.stderr.close()
